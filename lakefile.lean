797293033112337f3dc3b5455e61bc3018793bbe
Matthew Ballard <matt@mrb.email>
2022-10-18 13:19:41 -0400

    update lakefile and test again

--- lakefile.lean
+++ lakefile.lean
@@ -9,11 +9,8 @@ lean_lib Sets {
   -- add library configuration options here
 }
 
+@[defaultTarget]
 lean_lib Hw9 {
   -- add library configuration options here
 }
 
-@[defaultTarget]
-lean_exe hw9 {
-  root := `Main
-}
